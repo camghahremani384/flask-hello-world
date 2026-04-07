01ce530d214479a1e4d17435f845318a9e9ba8de
camghahremani384 <Cameron.Ghahremani@colorado.edu>
2026-04-07 15:46:52 +0000

    added db_select route

--- app.py
+++ app.py
@@ -44,4 +44,37 @@ def inserting():
         ''')
     conn.commit()
     conn.close()
-    return "Basketball Table Successfully Populated"
+    return "Basketball Table Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://labdb384_user:maLFyL59ww9g1kZSa4HXIPI8fLiMIhvK@dpg-d7ahobmuk2gs73cfvh9g-a/labdb384")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ""
+    response_string += "<table>"
+    for player in records:
+        response_string += "<tr>"
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
